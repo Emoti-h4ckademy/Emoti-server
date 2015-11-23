cf64465f6d845690eb1f9082246180b327d8d777
carlosbkm <carlos.garcia.mejias@gmail.com>
2015-11-23 14:19:00 +0100

    Updated app.js to include 'demo' environment

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var exphbs        = require('express-handlebars');
 
 
 var app = express(),
-    dev = app.get('env') === 'development';
+    dev = app.get('env') === 'development' || 'demo';
 
 app.use(bodyParser.json({limit: '2mb'}));
 app.use(bodyParser.urlencoded({limit: '2mb', extended: true}));
