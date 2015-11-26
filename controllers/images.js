8eec6ecc8288e9737e72bd1bd52acb2b25b3c2e1
carlosbkm <carlos.garcia.mejias@gmail.com>
2015-11-26 12:14:21 +0100

    Commented out console.log

--- controllers/images.js
+++ controllers/images.js
@@ -214,7 +214,7 @@ exports.getImageByMonth = function (month, callback) {
     Image.find({'date' : {'$gte': new Date(2015, month, 1), '$lt': new Date(2015, month + 1, 3)}},
         'username mainemotion emotions date',
         {$sort: { 'date' : 'ascending' } },function (err, images) {
-        console.log("Number of images: " + images) // Space Ghost is a talk show host.
+        //console.log("Number of images: " + images)
         callback(err, images);
     });
 };
